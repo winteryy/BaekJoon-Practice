12189e449c327f49007e64f9269c178a9c56e901
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-01-30 14:10:14 +0900

    codekata 16

---
+++ codekata16.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    long long cur = x;
+    for(int i=0; i<n; i++) {
+        answer.push_back(cur);
+        cur += x;
+        
+    }
+    return answer;
+}
