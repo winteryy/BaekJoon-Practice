f3e5f123c93956fbff0c96dce3e64757a448f146
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-03-22 10:17:43 +0900

    algorithm

--- codekata68.kt
+++ codekata68.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun solution(ingredient: IntArray): Int {
         var answer: Int = 0
-        
+
         var current = mutableListOf<Int>()
 
         for(num in ingredient) {
@@ -11,7 +11,7 @@ class Solution {
                 current[current.size-3]==2 && current[current.size-4]==1) {
                 answer++
                 for(i in 1..4) {
-                    current.removeAt(current.lastIndex)
+                    current.removeLast()
                 }
             }
         }
