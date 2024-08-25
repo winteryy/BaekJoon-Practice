78d7edd7386fc1e3071215d19e343aa5b05ef60c
Lee HuiGwang <lhk9805@gmail.com>
2024-08-25 22:37:23 +0900

    boj 13460

---
+++ 13460.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef pair<int, int> point;
+typedef pair<bool, point> ball_result;
+
+int n, m;
+int minDepth = 11;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+ball_result moveBall(vector<vector<char> >& board, point p, int dir, bool isRed) {
+    int curY = p.first;
+    int curX = p.second;
+
+    while(board[curY+dy[dir]][curX+dx[dir]] == '.') {
+        curY += dy[dir];
+        curX += dx[dir];
+    }
+
+    board[p.first][p.second] = '.';
+    if(board[curY+dy[dir]][curX+dx[dir]] == 'O') {
+        return {true, {curY+dy[dir], curX+dx[dir]}};
+    } else {
+        board[curY][curX] = isRed ? 'R' : 'B';
+        return {false, {curY, curX}};
+    }
+}
+
+void solve(vector<vector<char> > board, point blue, point red, int depth) {
+    if(depth>=minDepth) return;
+
+    for(int i=0; i<4; i++) {
+        vector<vector<char> > newBoard(board);
+        
+        ball_result redResult, blueResult;
+        switch(i) {
+            case 0: {
+                if(blue.second>red.second) {
+                    blueResult = moveBall(newBoard, blue, i, false);
+                    redResult = moveBall(newBoard, red, i, true);
+                } else {
+                    redResult = moveBall(newBoard, red, i, true);
+                    blueResult = moveBall(newBoard, blue, i, false);
+                }
+                break;
+            }
+
+            case 1: {
+                if(blue.first>red.first) {
+                    blueResult = moveBall(newBoard, blue, i, false);
+                    redResult = moveBall(newBoard, red, i, true);
+                } else {
+                    redResult = moveBall(newBoard, red, i, true);
+                    blueResult = moveBall(newBoard, blue, i, false);
+                }
+                break;
+            }
+
+            case 2: {
+                if(blue.second<red.second) {
+                    blueResult = moveBall(newBoard, blue, i, false);
+                    redResult = moveBall(newBoard, red, i, true);
+                } else {
+                    redResult = moveBall(newBoard, red, i, true);
+                    blueResult = moveBall(newBoard, blue, i, false);
+                }
+                break;
+            }
+
+            case 3: {
+                if(blue.first<red.first) {
+                    blueResult = moveBall(newBoard, blue, i, false);
+                    redResult = moveBall(newBoard, red, i, true);
+                } else {
+                    redResult = moveBall(newBoard, red, i, true);
+                    blueResult = moveBall(newBoard, blue, i, false);
+                }
+                break;
+            }
+        }
+
+        if(blueResult.first) continue;;
+        if(redResult.first) {
+            minDepth = min(minDepth, depth);
+            continue;
+        }
+        if(blueResult.second == blue && redResult.second == red) continue;
+        if(depth < 10) {
+            solve(newBoard, blueResult.second, redResult.second, depth+1);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    vector<vector<char> > board = vector<vector<char> >(n, vector<char>(m));
+    point red, blue;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> board[i][j];
+            if(board[i][j] == 'R') {
+                red = {i, j};
+            } else if(board[i][j] == 'B') {
+                blue = {i, j};
+            }
+        }
+    }
+
+    solve(board, blue, red, 1);
+
+    cout << (minDepth==11?-1:minDepth);
+}
