f4152cb5c6b922a01794c2cbbebd4aeab46fcc37
Lee HuiGwang <lhk9805@gmail.com>
2024-09-01 23:21:49 +0900

    boj 2143

---
+++ 2143.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+int a[1000];
+int b[1000];
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int t, n, m;
+
+    cin >> t >> n;
+
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    cin >> m;
+
+    for(int i=0; i<m; i++) {
+        cin >> b[i];
+    }
+
+    unordered_map<int, int> aMap, bMap;
+
+    for(int i=0; i<n; i++) {
+        int sum = 0;
+        for(int j=i; j<n; j++) {
+            sum+=a[j];
+            aMap[sum]++;
+        }
+    }
+
+    for(int i=0; i<m; i++) {
+        int sum = 0;
+        for(int j=i; j<m; j++) {
+            sum+=b[j];
+            bMap[sum]++;
+        }
+    }
+
+    long long result = 0;
+
+    for(auto e: aMap) {
+        auto bElement = bMap.find(t-e.first);
+        if(bElement!=bMap.end()) {
+            result += (long long)e.second*(*bElement).second;
+        }
+    }
+
+    cout << result;
+}
