8aa9ef845c128e18fb4cb5431dbdca5a2bffe480
Lee HuiGwang <lhk9805@gmail.com>
2024-09-11 20:36:01 +0900

    boj 17404

--- 17404.cpp
+++ 17404.cpp
@@ -9,6 +9,9 @@ int startWithGreen[1000][3];
 int startWithBlue[1000][3];
 
 int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
     int n;
     cin >> n >> startWithRed[0][0] >> startWithGreen[0][1] >> startWithBlue[0][2];
     startWithRed[0][1] = INF_COST;
@@ -19,30 +22,29 @@ int main() {
     startWithBlue[0][1] = INF_COST;
 
     int redCost, greenCost, blueCost;
-    for(int i=1; i<n-1; i++) { //마지막 원소는 따로 계산
+    for(int i=1; i<n; i++) { //마지막 원소는 따로 계산
         cin >> redCost >> greenCost >> blueCost;
 
-        startWithRed[i][0] = min(startWithRed[i-1][1] + redCost, startWithRed[i-1][2] + redCost);
-        startWithRed[i][1] = min(startWithRed[i-1][0] + greenCost, startWithRed[i-1][2] + greenCost);
-        startWithRed[i][2] = min(startWithRed[i-1][0] + blueCost, startWithRed[i-1][1] + blueCost);
+        startWithRed[i][0] = min(startWithRed[i-1][1], startWithRed[i-1][2]) + redCost;
+        startWithRed[i][1] = min(startWithRed[i-1][0], startWithRed[i-1][2]) + greenCost;
+        startWithRed[i][2] = min(startWithRed[i-1][0], startWithRed[i-1][1]) + blueCost;
 
-        startWithGreen[i][0] = min(startWithGreen[i-1][1] + redCost, startWithGreen[i-1][2] + redCost);
-        startWithGreen[i][1] = min(startWithGreen[i-1][0] + greenCost, startWithGreen[i-1][2] + greenCost);
-        startWithGreen[i][2] = min(startWithGreen[i-1][0] + blueCost, startWithGreen[i-1][1] + blueCost);
+        startWithGreen[i][0] = min(startWithGreen[i-1][1], startWithGreen[i-1][2]) + redCost;
+        startWithGreen[i][1] = min(startWithGreen[i-1][0], startWithGreen[i-1][2]) + greenCost;
+        startWithGreen[i][2] = min(startWithGreen[i-1][0], startWithGreen[i-1][1]) + blueCost;
 
-        startWithBlue[i][0] = min(startWithBlue[i-1][1] + redCost, startWithBlue[i-1][2] + redCost);
-        startWithBlue[i][1] = min(startWithBlue[i-1][0] + greenCost, startWithBlue[i-1][2] + greenCost);
-        startWithBlue[i][2] = min(startWithBlue[i-1][0] + blueCost, startWithBlue[i-1][1] + blueCost);                
+        startWithBlue[i][0] = min(startWithBlue[i-1][1], startWithBlue[i-1][2]) + redCost;
+        startWithBlue[i][1] = min(startWithBlue[i-1][0], startWithBlue[i-1][2]) + greenCost;
+        startWithBlue[i][2] = min(startWithBlue[i-1][0], startWithBlue[i-1][1]) + blueCost;                
     }   
 
-    cin >> redCost >> greenCost >> blueCost;
-    int candidateList[12] = {
-        startWithRed[n-2][0]+greenCost, startWithRed[n-2][0]+blueCost, startWithRed[n-2][1]+blueCost, startWithRed[n-2][2]+greenCost,
-        startWithGreen[n-2][0]+blueCost, startWithGreen[n-2][1]+redCost, startWithGreen[n-2][1]+blueCost, startWithGreen[n-2][2]+redCost,
-        startWithBlue[n-2][0]+greenCost, startWithBlue[n-2][1]+redCost, startWithBlue[n-2][2]+redCost, startWithBlue[n-2][2]+greenCost,
+    int candidateList[6] = {
+        startWithRed[n-1][1], startWithRed[n-1][2],
+        startWithGreen[n-1][0], startWithGreen[n-1][2],
+        startWithBlue[n-1][0], startWithBlue[n-1][1]
     };
 
-    int result = *min_element(candidateList, candidateList+12);
+    int result = *min_element(candidateList, candidateList+6);
 
     cout << result;
 }
