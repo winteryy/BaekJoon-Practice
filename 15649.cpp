f3e6558f0c5ef35bf4987d66c814d3fbfe472c31
Lee HuiGwang <lhk9805@gmail.com>
2025-01-30 18:31:08 +0900

    boj 15649

---
+++ 15649.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int n, m;
+int curArr[8];
+bool isVisited[9];
+
+void dfs(int depth) {
+    if(depth == m) {
+        for(int i=0; i<m; i++) {
+            cout << curArr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    for(int i=1; i<=n; i++) {
+        if(isVisited[i]==false) {
+            isVisited[i] = true;
+            curArr[depth] = i;
+            dfs(depth+1);
+            isVisited[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    memset(isVisited, false,  n+1);
+    dfs(0);
+}
